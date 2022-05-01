169994b0a9b3add5792921f11625f6cd1678ec30
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-04-30 22:31:09 -0300

    plantilla

--- Projecto1/Projecto1/views.py
+++ Projecto1/Projecto1/views.py
@@ -1,17 +1,34 @@
+
 from django.http import HttpResponse
 import datetime
+from django.template import Template, Context
 
+class Persona(object):
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
 
 def saludo (request):#primera vista
-    parafo = """<html>
-    <body>
-    <p>Esto es un parafo</p>
-    <p> Soy naomi milagro poclava esta es mi pagina web </p>
-    </body>
-    </html>"""
 
-    titulo = '<html><body><h1>Pagina hecha en Diango</h1></body></html>'
-    return HttpResponse(parafo)
+    p1 = Persona("Juan", "Diaz")
+    #nombre ="Juan"
+    #apellido = "Serrano"
+    fecha_actual = datetime.datetime.now()
+
+
+
+    doc_externo = open("C:/Users/milag/OneDrive/Escritorio/ProyectosDjango/Projecto1/Projecto1/Plantillas/miPlantilla.html")
+
+    
+    plt = Template(doc_externo.read())
+
+    doc_externo.close()
+
+    ctx = Context({"nombre_persona": p1.nombre, "apellido_persona": p1.apellido, "fecha": fecha_actual})
+
+    documento = plt.render(ctx)
+
+    return HttpResponse(documento)
 
 def despedida(request):
     return HttpResponse("Hasta luego alumnos de django, chau amigos")
